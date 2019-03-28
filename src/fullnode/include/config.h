b52255f296c8f1151d04b4e4372f4c2ffbba9fcd
rongwl <michael_air_rong@163.com>
2019-03-28 18:42:32 +0800

    add config.cpp/config.h

---
+++ src/fullnode/include/config.h
@@ -0,0 +1,43 @@
+#ifndef BTCLITE_FULLNODE_CONFIG_H
+#define BTCLITE_FULLNODE_CONFIG_H
+
+#include "environment.h"
+#include "chain/include/params.h"
+#include "network/include/params.h"
+#include "util.h"
+
+
+#define FULLNODE_OPTION_CONNECT  "connect"
+#define FULLNODE_OPTION_LISTEN   "listen"
+#define FULLNODE_OPTION_DISCOVER "discover"
+#define FULLNODE_OPTION_DNSSEED  "dnsseed"
+
+#define DEFAULT_CONFIG_FILE     "btc-fullnode.conf"
+#define DEFAULT_DATA_DIR        ".btc-fullnode"
+
+#define DEFAULT_LISTEN    "1"
+#define DEFAULT_DISCOVER  "1"
+#define DEFAULT_DNSSEED   "1"
+
+
+class FullNodeArgs : public Args {
+public:
+	bool Init(int argc, const char* const argv[]);
+	bool InitParameters();
+
+private:
+	bool Parse(int argc, const char* const argv[]);
+	void PrintUsage();
+};
+
+class FullNodeDataFiles : public DataFiles {
+public:
+	using DataFiles::DataFiles;
+	
+	bool Init(const std::string& path, const std::string& config_file);
+	static fs::path DefaultDataDirPath();
+	
+};
+
+
+#endif // BTCLITE_FULLNODE_CONFIG_H
