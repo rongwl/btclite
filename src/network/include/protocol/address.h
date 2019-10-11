8ab471781fbd25845982bde6f0a8a2a5a974abf2
rongwl <michael_air_rong@163.com>
2019-10-11 13:12:59 +0800

    clean some inactive code

--- src/network/include/protocol/address.h
+++ src/network/include/protocol/address.h
@@ -13,52 +13,7 @@ namespace btclite {
 namespace network {
 namespace protocol {
 
-struct NetAddr {
-    uint32_t timestamp;
-    uint64_t services;
-    IpAddr ip;
-    uint16_t port;
-    
-    //-------------------------------------------------------------------------
-    bool IsValid() const;
-    void Clear();
-    size_t SerializedSize() const
-    {
-        return sizeof(timestamp) + sizeof(services) + ip.size() + sizeof(port);
-    }
-    
-    //-------------------------------------------------------------------------
-    template <typename Stream>
-    void Serialize(Stream& out) const;
-    template <typename Stream>
-    void Deserialize(Stream& in);
-    
-    //-------------------------------------------------------------------------
-    bool operator==(const NetAddr& b) const;
-    bool operator!=(const NetAddr& b) const;
-};
 
-template <typename Stream>
-void NetAddr::Serialize(Stream& out) const
-{
-    Serializer<Stream> serializer(out);
-    
-    serializer.SerialWrite(timestamp);
-    serializer.SerialWrite(services);
-    serializer.SerialWrite(ip);
-    serializer.SerialWrite(port);
-}
-
-template <typename Stream>
-void NetAddr::Deserialize(Stream& in)
-{
-    Deserializer<Stream> deserializer(in);
-    
-    deserializer.SerialRead(&timestamp);
-    deserializer.SerialRead(&services);
-    deserializer.SerialRead(&ip);
-    deserializer.SerialRead(&port);
-}
 
 } // namespace protocol
 } // namespace network
--- src/network/src/protocol/address.cpp
+++ src/network/src/protocol/address.cpp
@@ -5,34 +5,7 @@ namespace btclite {
 namespace network {
 namespace protocol {
 
-bool NetAddr::IsValid() const
-{
-    return (timestamp != 0) ||
-           (services != 0) ||
-           (ip != kNullIp) ||
-           (port != 0);
-}
 
-void NetAddr::Clear()
-{
-    timestamp = 0;
-    services = 0;
-    ip = kNullIp;
-    port = 0;
-}
-
-bool NetAddr::operator==(const NetAddr& b) const
-{
-    return (timestamp == b.timestamp) &&
-           (services == b.services) &&
-           (ip == b.ip) &&
-           (port == b.port);
-}
-
-bool NetAddr::operator!=(const NetAddr& b) const
-{
-    return !(*this == b);
-}
 
 } // namespace protocol
 } // namespace network
